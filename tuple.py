a56c25d543323c2a8bc71425cbbf599a5ad07fd4
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-04-14 19:35:28 +0300

    tuples work

---
+++ tuple.py
@@ -0,0 +1,32 @@
+# Accessing items in a tuple
+
+fruits=("Orange","Mango","Apple","Pineapple")
+print(fruits)
+print(fruits[3])
+
+# Removing items in a tuple
+
+fruits= ("Orange",23,"Apple","Pineapple")
+print(fruits)
+
+
+# Adding items in a tuple
+numbers= (1,2,3,4,5)
+print(numbers)
+
+# Converting a tuple to a list
+numbers=[1,2,3,4,5]
+print(numbers)
+
+# Concating a list
+
+numbers=[1,2,3,4,5]
+number=[6,7,8,9]
+
+res = numbers + number
+print(res)
+
+# Converting a list back to a tuple
+
+numbers= (1,2,3,4,5,6,7,8,9)
+print(numbers)
